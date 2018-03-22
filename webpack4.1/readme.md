fdd677d4e37a9357d351db32e43d3fd04aa0dfbd
823844596 <823844596@qq.com>
2018-03-22 22:33:30 +0800

    file-loader/url-loader

---
+++ webpack4.1/readme.md
@@ -0,0 +1,14 @@
+#webpack n文件处理
+文件处理类型：
+1 图片处理
+    1）css中如何引入图片
+        使用插件：file-loader
+    2）自动合成雪碧图
+        使用插件：postcss-sprites
+    3）压缩图片
+        使用插件：url-loader
+    4）小图片Base64编码
+        使用插件：img-loader
+
+2 字体文件
+3 第三方JS库
---
+++ webpack4.1/src/app.js
@@ -0,0 +1,36 @@
+import base from './css/base.css';
+import './css/common.css';
+
+// 通过使用style-loader/useable,可使用base对象方法
+// base.use(); //使用css 
+// base.unuse(); //不使用css
+
+// var flag = false;
+// setInterval(function(){
+//     if (!flag) {
+//         base.use();
+//         flag = true;
+//     } else {
+//         base.unuse();
+//         flag = false;
+//     }
+// }, 1000)
+
+const app = document.getElementById('app');
+app.innerHTML = '<div class="box "></div>'
+// app.innerHTML = '<div class="' + base.box + '"></div>'
+// app.innerHTML = '<div class="' + base.bigBox + '"></div>'
+
+// 注意：import与（）中间不能有空格
+// import(/*webpackChunkName:'a'*/ './components/a').then(function(){
+//     console.log('异步加载js,js中引入css');
+// })
+
+import {a} from './common/util';
+console.log(a());
+
+
+// import {chunk} from 'lodash';
+import {chunk} from 'lodash-es';
+
+console.log(chunk([1,2,3], 5))
